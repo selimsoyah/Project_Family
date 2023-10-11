aa8a638e2b0f04e1bb1b955163739f75913e512e
salim soyah <selimsoyah86@gmail.com>
2023-10-11 14:04:33 +0100

    CLI conf

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React NOoOOOOWjhjkghjgk
+          Learn React NOoOOOOWjhjkghjgks
         </a>
       </header>
     </div>
